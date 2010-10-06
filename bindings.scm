6d9c859d54961aad64491f5d3d52677a379f6515
Stephen Eilert <spedrosa@gmail.com>
2010-10-05 22:40:56 -0300

    - Adding the bindings

--- bindings.scm
+++ bindings.scm
@@ -29,25 +29,25 @@
 (bind "int mkd_generatehtml(MMIOT *doc, FILE *out)")
 
 ;; Returns (in 'text') a pointer to the compiled HTML document, and (in the return code) the size of that document
-(bind "int mkd_document(MMIOT *doc, char **text")
+(bind "int mkd_document(MMIOT *doc, char **text)")
 
 ;; Allocates a buffer and populates it with any style blocks found in the document
-(bind "int mkd_css(MMIOT *doc, char **out")
+(bind "int mkd_css(MMIOT *doc, char **out)")
 
 ;; Prints any style blocks in the document
-(bind "int mkd_generatecss(MMIOT *doc, FILE *out")
+(bind "int mkd_generatecss(MMIOT *doc, FILE *out)")
 
 ;; Allocates a string, populates it with a table of contents, assigns it to out, and returns the length of the string
-(bind "int mkd_toc(MMIOT *doc, char **out")
+(bind "int mkd_toc(MMIOT *doc, char **out)")
 
 ;;; To get a table of contents, you must (compile) the document with the MKD_TOC flag (described below)
 
 ;; Writes a table of contents to out, other than writing to a string, it operates exactly like mkd_toc
-(bind "int mkd_generatetoc(MMIOT *doc, FILE *out")
+(bind "int mkd_generatetoc(MMIOT *doc, FILE *out)")
 
 ;; Prints a block structure diagram of a compiled document
-(bind "int mkd_dump(MMIOT *doc, FILE *f, int flags, char *title")
+(bind "int mkd_dump(MMIOT *doc, FILE *f, int flags, char *title)")
 
 ;; Releases the MMIOT allocated for the document
-(bind "void mkd_cleanup(MMIOT *doc")
+(bind "void mkd_cleanup(MMIOT *doc)")
 
