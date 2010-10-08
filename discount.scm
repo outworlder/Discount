b85a760f2190d6838505d2e9ada03155249b3993
Stephen Eilert <spedrosa@gmail.com>
2010-10-08 19:42:55 -0300

    - Generating markdown files in-memory

--- discount.scm
+++ discount.scm
@@ -1,51 +1,14 @@
 (declare (unit discount))
 
-(define (sys-open-file filename #!key (mode "r+"))
-  (let ([result (fopen filename mode)])
-    (if (equal? result #f)
-	(begin
-	  (perror "")
-	  result)
-	result)))
-
-(define (sys-close-file filename)
-  (let ([result (fclose filename)])
-    (if (equal? result #f)
-	(begin
-	  (perror "")
-	  result)
-	result)))
-
-;;; Generates a markdown object
-  ;; (define (markdown-document data)
-  ;;   ;;TODO
-  ;;   ))
-
-;;; Reads a markdown document from a file
-(define (parse-markdown-file input-file)
-  (let ([file (sys-open-file input-file mode: "r")])
-    (mkd_in file 0)))
-
-;;; Reads the input string and returns a markdown-document
-(define (parse-markdown input)
-  (mkd_string input (string-length input) 0))
-
-;;; Generates HTML to file
-(define (generate-html document filename)
-  (let* ([file (sys-open-file filename mode: "w")])
-    (if file
-	(begin
-	  (markdown document file 0)
-	  (sys-close-file file)))))
-
-;; (generate-html
-;;  (parse-markdown "<<TEST
-;; Download
-;; --------
-
-;; [Markdown 1.0.1][dl] (18 KB) -- 17 Dec 2004
-
-;; [dl]: http://daringfireball.net/projects/downloads/Markdown_1.0.1.zip
-;; TEST"
-;; 		 ) "test.html")
-
+(define (markdown-generate-in-memory doc)
+  (let-location ([out c-string*])
+		(let ([rv (discount-mkd-document doc (location out))])
+		       (let ([out out])
+			 out))))
+  
+(define (markdown input #!key (flags 0))
+  (let ([document (discount-mkd-string input (string-length input) flags)])
+    (discount-mkd-compile! document flags)
+    (markdown-generate-in-memory document)))
+	 
+    
--- test.scm
+++ test.scm
@@ -7,15 +7,14 @@
 (use extras)
 (use utils)
 
-(test-group "Markdown file i/o"
-	    (test "Should generate an html file from the markdown input file"
-		  "test.html"
-		  (begin
-		    (generate-html (parse-markdown-file "test.markdown") "test.html")
-		    (file-exists? "test.html")))
-	    (test "The generated file should contain the corrent HTML markup"
+(test-group "Markdown in-memory processing"
+	    (test "Should return a html string from a given markdown string"
 		  "<h2>Download</h2>
 
-<p><a href=\"http://daringfireball.net/projects/downloads/Markdown_1.0.1.zip\">Markdown 1.0.1</a> (18 KB) &mdash; 17 Dec 2004</p>
-" (read-all "test.html")))
+<p><a href=\"http://daringfireball.net/projects/downloads/Markdown_1.0.1.zip\">Markdown 1.0.1</a> (18 KB) &mdash; 17 Dec 2004</p>" (markdown "Download
+--------
 
+[Markdown 1.0.1][dl] (18 KB) -- 17 Dec 2004
+
+[dl]: http://daringfireball.net/projects/downloads/Markdown_1.0.1.zip
+")))
