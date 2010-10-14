d20445bd27899d1d089d69a6c8323a6fa553e375
Stephen Eilert <spedrosa@gmail.com>
2010-10-14 10:09:53 -0300

    - Fixing missing parenthesis

--- discount.scm
+++ discount.scm
@@ -2,15 +2,16 @@
 	'markdown
 	(import scheme chicken foreign)
 
-(define (markdown-generate-in-memory doc)
-  (print "\n omsdgsd fsdf --------------------------------------------------" doc)
-  (let-location ([out c-string*])
-		(let ([rv (discount-mkd-document doc (location out))])
-		       (let ([out out])
-			 out))))
-  
-(define (markdown input #!key (flags 0))
-  (let ([document (discount-mkd-string input (string-length input) flags)])
-    (discount-mkd-compile! document flags)
-    (markdown-generate-in-memory document)))
-	 
+	(include "bindings")
+	
+	(define (markdown-generate-in-memory doc)
+	  (let-location ([out c-string*])
+			(let ([rv (discount-mkd-document doc (location out))])
+			  (let ([out out])
+			    out))))
+	
+	(define (markdown input #!key (flags 0))
+	  (let ([document (discount-mkd-string input (string-length input) flags)])
+	    (discount-mkd-compile! document flags)
+	    (markdown-generate-in-memory document))))
+
